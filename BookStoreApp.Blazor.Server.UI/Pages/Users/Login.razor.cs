62b7a52b1f3c0475666a667332c71ad0f9456f6e
KLNHOMEALONE <KLNHOMEALONE@gmail.com>
2023-01-25 15:44:47 +0300

    Refactoring Login page

---
+++ BookStoreApp.Blazor.Server.UI/Pages/Users/Login.razor.cs
@@ -0,0 +1,42 @@
+﻿using BookStoreApp.Blazor.Server.UI.Services.Authentication;
+using BookStoreApp.Blazor.Server.UI.Services.Base;
+using Microsoft.AspNetCore.Components;
+
+namespace BookStoreApp.Blazor.Server.UI.Pages.Users
+{
+    public partial class Login
+    {
+        [Inject] 
+        IAuthenticationService AuthService { get; set; }
+
+        [Inject] 
+        NavigationManager NavManager { get; set; }
+
+        LoginUserDto LoginModel = new LoginUserDto();
+        string message = string.Empty;
+        public async Task HandleLogin()
+        {
+            try
+            {
+                var response = await AuthService.AuthenticateAsync(LoginModel);
+
+                if (response)
+                {
+                    NavManager.NavigateTo("/");
+                }
+
+                message = "Invalid Credentials, Please Try Again";
+            }
+            catch (ApiException ex)
+            {
+                if (ex.StatusCode >= 200 && ex.StatusCode <= 299)
+                {
+
+                }
+
+                message = ex.Response;
+            }
+        }
+
+    }
+}
